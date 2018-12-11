7ace495de7aa30107f485f973a6614fd379f20d5
wangjunM <wangjun8@xiaomi.com>
2018-12-11 16:09:02 +0800

    个人主体类小程序不能包含视频，改成gif

--- 时代见证者/pages/page1/page1.js
+++ 时代见证者/pages/page1/page1.js
@@ -6,27 +6,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-    con:false,
-    imgs: [
-      {
+    con: false,
+    imgs: [{
         url: app.globalData.host + "videos/index.gif"
+      },
+      {
+        url: "https://i.loli.net/2018/12/11/5c0f643f051dc.gif"
       }
     ],
-    videoSrc: "http://pixwlubu5.bkt.clouddn.com/%E7%89%87%E5%A4%B4.mp4"
+    imgSrc: "../../resources/image/index.png"
+    //videoSrc: "http://pixwlubu5.bkt.clouddn.com/%E7%89%87%E5%A4%B4.mp4"
   },
 
-  end:function(){
+  end: function() {
     wx.redirectTo({
       url: '../page2/page2',
     })
   },
-  touchVideo:function(){
+  touchVideo: function() {
     wx.redirectTo({
       url: '../page2/page2',
     })
   },
 
-  play: function(){
+  play: function() {
     console.log("play");
   },
 
@@ -41,9 +44,31 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
     wx.redirectTo({
       url: '../page2/page2'
     })
+  },
+
+  // onReady: function() {
+  //   setTimeout(function(){
+  //     wx.redirectTo({
+  //       url: '../page2/page2'
+  //     })
+  //   },5000)
+  // }
+  onLoad: function() {
+    wx.showLoading({
+      title: '精彩即将开始...',
+    })
+  },
+
+  imgLoaded: function() {
+    wx.hideLoading();
+    setTimeout(function() {
+      wx.redirectTo({
+        url: '../page2/page2'
+      })
+    }, 5000)
   }
 })
